65228a276e22d63dea253f0b05e2a7f3daad4299
Himanshu77x <himanshu77b@gmail.com>
2025-02-13 16:04:30 +0530

    Added Render domain to ALLOWED_HOSTS

--- sh/settings.py
+++ sh/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-f3ch88-7*1=8(n*p2o#_uuncab$g!=l=1gz-)ezxonwg-)6gcd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['shoes-3.onrender.com']
+
 
 
 
